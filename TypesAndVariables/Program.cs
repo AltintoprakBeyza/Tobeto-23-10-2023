07d9fc8c4632d8393ecc18747cc5d24343a02400
AltintoprakBeyza <cicurit@gmail.com>
2023-10-24 16:13:38 +0300

    C# Dersleri 5: Integer Veri Tipi

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//C# Dersleri 5: Integer Veri Tipi
+//integer veri tipi: Değer tiplerinden(Value Types) bir tanesidir.
+//int - veri tipi, number1- variable(değişken)
+int number1 = 10;
+Console.WriteLine("Number1 is {0}",number1);
+Console.ReadLine();
+
+//Bütün veri tiplerinin sınırları vardır.integer veri tipinin sınırı -2147483648 ile 2147483647 arasındaki sayılardır.
