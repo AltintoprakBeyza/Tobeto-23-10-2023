edd832d42e7662099d82ff2da19176ebafc40401
AltintoprakBeyza <cicurit@gmail.com>
2023-10-27 13:08:55 +0300

    Adding homework C# Lessons: 46-47

---
+++ Inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer
+                {
+                    FirstName = "Engin"
+                },
+                new Student
+                {
+                    FirstName="Derin"
+                },
+                new Person
+                {
+                    FirstName ="Salih"
+                }
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+
+    class Customer:Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
