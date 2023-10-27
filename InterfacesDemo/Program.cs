4a245b5b8fa67cb8ad9e9519dcfefe4ff8046d85
AltintoprakBeyza <cicurit@gmail.com>
2023-10-27 12:44:02 +0300

    Adding homework C# Lessons: 45

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,91 @@
+﻿
+namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
